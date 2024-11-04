12b8db1bb4456f26c8e62839334cc2ab0aef37af
basant <basantamin@outlook.sa>
2024-11-04 05:18:57 +0300

    Initial Commit

---
+++ Mesbaha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mesbaha
+//
+//  Created by basant amin bakir on 04/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mesbaha/MesbahaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MesbahaApp.swift
+//  Mesbaha
+//
+//  Created by basant amin bakir on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MesbahaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
